f933cbf8f14e2c658cb6a7d02d68bf253db8ef4b
Melli <Melanie.hauser@campus.lmu.de>
2020-01-12 14:39:41 +0100

    add UDP communication

--- Assets/Scripts/Configuration.cs
+++ Assets/Scripts/Configuration.cs
@@ -5,6 +5,11 @@ using System.IO;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 public class Configuration : MonoBehaviour
 {
@@ -14,10 +19,14 @@ public class Configuration : MonoBehaviour
     public TextAsset letterFile;
     public float width;
     public float height;
-    public int id;
+    public int id = -1;
     GameObject inputField;
     Text text;
     GameObject canvas;
+
+    int port = 9900;
+    UdpClient client;
+    Thread appThread;
 
     void Awake()
     {
@@ -26,6 +35,9 @@ public class Configuration : MonoBehaviour
         height = Screen.height;
         inputField = GameObject.FindGameObjectWithTag("ID");
         text = GameObject.FindGameObjectWithTag("Text").GetComponent<Text>();
+        appThread = new Thread(new ThreadStart(ReceiveData));
+        appThread.IsBackground = true;
+        appThread.Start();
     }
 
     void Start()
@@ -63,4 +75,40 @@ public class Configuration : MonoBehaviour
         }
     }
 
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+        client.Client.Blocking = false;
+        while (true)
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+                string text = Encoding.UTF8.GetString(data);
+                if (id == -1) int.TryParse(text, out id); 
+                if (text != id.ToString()) Application.Quit();
+                print("text: " + text);
+                print("id: " + id);
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        stopThread();
+    }
+
+    private void stopThread()
+    {
+        if (appThread.IsAlive)
+        {
+            appThread.Abort();
+        }
+        client.Close();
+    }
 }
